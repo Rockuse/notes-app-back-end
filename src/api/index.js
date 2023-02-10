f6e780254218910ca3c551a636d13ab873586ba6
Fahmi Muzakki <fahmirockman@gmail.com>
2023-02-10 17:47:28 +0700

    add exports service,api and validator

--- src/api/index.js
+++ src/api/index.js
@@ -1,2 +1,2 @@
 // eslint-disable-next-line global-require
-module.exports = [require('./notes'), require('./users'), require('./authentications'), require('./collaborations')];
+module.exports = [require('./notes'), require('./users'), require('./authentications'), require('./collaborations'), require('./exports')];
